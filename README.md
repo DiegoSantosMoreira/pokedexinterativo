be42606d0fe2939816d6eca3b43ad4fcb7ab3a28
Diego Moreira <diegouezotads@gmail.com>
2023-10-19 18:27:16 -0300

    feat: updanting JS and CSS files and add README.md

---
+++ README.md
@@ -0,0 +1,30 @@
+# 💡 POKEDEX INTERATIVO 
+
+---
+
+Apresento-lhes uma Pokedex diferente. A Pokedex interativa 😅
+
+Nesta Pokedex o usuário clica, e vai verificando o pokemon capturado.
+---
+
+### 👨🏾‍💻 DESAFIOS E APRENDIZADOS
+
+- Consumo da API pokeapi.co;
+- Lógica de programação;
+- Trabalhando com *promisses* (métodos *fecth e then*)
+
+---
+
+### 🛠 TECNOLOGIAS UTILIZADAS
+
+ ![HTML5](https://img.shields.io/badge/HTML5-000?style=for-the-badge&logo=html5) 
+![CSS3](https://img.shields.io/badge/CSS3-000?style=for-the-badge&logo=css3&logoColor=264CE4)
+![JavaScript](https://img.shields.io/badge/JavaScript-000?style=for-the-badge&logo=javascript&color=000)
+
+---
+
+### 🚀 DEPLOY DO PROJETO
+
+Caso queira ter a experiência, clique no link abaixo 👇👇
+
+[pokedex interativo](https://diegosantosmoreira.github.io/pokedexinterativo/)
---
+++ js/script.js
@@ -0,0 +1,66 @@
+// Variáveis e seleção de itens 
+
+//const pokemonURL = `https://pokeapi.co/api/v2/pokemon/`
+const btnOpenBall = document.querySelector("#btn");
+const pokeballOpen = document.querySelector("#pokeball");
+const pokeballClosed = document.querySelector("#pokeball")
+const switchTitle = document.querySelector("#title");
+const namePokemonFromAPI = document.querySelector("#namepokemon");
+const typePokemonFromAPI = document.querySelector("#type");
+const imagePokemonFromAPI = document.querySelector("#pokemon-image");
+const pokemonContainer = document.getElementById("#show-pokemon");
+
+let backPoke = document.querySelector('#pokemon')
+
+let type;
+
+
+// Funções
+function switchImageAndTitle (){
+    pokeballOpen.setAttribute("src", "./assets/pokebalopen.png");
+    
+
+    setTimeout (switchPokeball, 900)
+    
+}
+
+const getPokemon = async () => {
+    const pokeURL = `https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random() * 150)}`
+
+    fetch (pokeURL)
+        .then(response => response.json())
+        .then(data => {
+
+            type = data.types[0].type.name
+
+            switchTitle.innerText = 'Você capturou ' + data.name;
+            namePokemonFromAPI.innerText =  'Name: ' + data.name;
+            typePokemonFromAPI.innerText =  'Type: ' + data.types.map(typeinfo => typeinfo.type.name).join(' | ');
+            // imagePokemonFromAPI.setAttribute("src", `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${data.id}.png`)
+            imagePokemonFromAPI.setAttribute("src", `https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${data.id}.svg`)
+
+            console.log(data)
+            console.log(type)
+            console.log(data.types.map(typeinfo => typeinfo.type.name).join(' | '))
+            
+            backPoke.classList.add(type)
+            imagePokemonFromAPI.classList.remove('hide')
+            console.log(backPoke)
+
+        })
+}
+
+function switchPokeball () {
+    pokeballClosed.setAttribute("src", "./assets/Poké_Ball_icon.svg.png");
+    pokeballClosed.classList.add('pokeball')
+    return pokeballClosed;
+}
+
+
+// Eventos botão
+btnOpenBall.addEventListener("click", () => {
+    switchImageAndTitle();
+    getPokemon();
+    backPoke.classList.remove(type)
+})
+
